59953399411d84b39be2bd2f219f0d6aefd98536
GJBLUE <895733091@qq.com>
2016-05-19 20:15:27 +0800

    studying to 8 chapter

---
+++ flask_demo/app/views.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}
+    posts = [  # fake array of posts
+                {
+                'author': {'nickname': 'John'}, 
+                'body': 'Beautiful day in Portland!' 
+                },
+                {
+                'author': {'nickname': 'Susan'}, 
+                'body': 'The Avengers movie was so cool!' 
+                }
+            ]
+    return render_template('index.html',
+                          title='Home',
+                          user=user,
+                          posts=posts)
